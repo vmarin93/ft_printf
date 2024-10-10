84e63a71a4d9131b7de5ef467da1e6309bce2956
Vasile Marin <vasilemarin93@gmail.com>
2024-10-10 10:58:19 +0100

    modified the word new to new_lst in 2 list functions, as the new word is a keyword in C

--- libft/ft_lstadd_front.c
+++ libft/ft_lstadd_front.c
@@ -12,8 +12,8 @@
 
 #include "libft.h"
 
-void	ft_lstadd_front(t_list **lst, t_list *new)
+void	ft_lstadd_front(t_list **lst, t_list *new_lst)
 {
-	new->next = *lst;
-	*lst = new;
+	new_lst->next = *lst;
+	*lst = new_lst;
 }
